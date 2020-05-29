634512b79005e2a6077832817f7239c510c50da5
whenofficial <fsmitchm@gmail.com>
2020-05-29 10:36:14 +0800

    Validation and handling

--- flask/src/repositories/user.py
+++ flask/src/repositories/user.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import load_only
 import bcrypt
 import string
 import random
+from werkzeug.exceptions import UnprocessableEntity
 
 
 class UserRepository:
@@ -12,6 +13,15 @@ class UserRepository:
     @staticmethod
     def create(username, email, password):
         """ Create a new user """
+
+        exists_username = bool(User.query.filter_by(username=username).first())
+        exists_email = bool(User.query.filter_by(email=email).first())
+        if exists_username:
+            raise UnprocessableEntity(description="USER_USERNAME_EXISTS")
+
+        if exists_email:
+            raise UnprocessableEntity(description="USER_EMAIL_EXISTS")
+
         
         hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         secret = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
