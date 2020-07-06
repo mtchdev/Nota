54e7992b1df80b4b8da2beaccd6a4ff893d1eb06
Mitch <me@miitch.io>
2020-07-06 11:18:00 +0800

    Tests

--- nota/src/app/components/app/inbox/inbox.component.spec.ts
+++ nota/src/app/components/app/inbox/inbox.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InboxComponent } from './inbox.component';
 import { SidebarDirectiveComponent } from 'app/directives/sidebar/sidebar.directive';
 import { NoteItemDirectiveComponent } from 'app/directives/notes/note-item.directive';
+import { EditorDirectiveComponent } from 'app/directives/notes/editor.directive';
+import { QuillModule } from 'ngx-quill';
 
 describe('InboxComponent', () => {
   let component: InboxComponent;
@@ -10,7 +12,8 @@ describe('InboxComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InboxComponent, SidebarDirectiveComponent, NoteItemDirectiveComponent ]
+      declarations: [ InboxComponent, SidebarDirectiveComponent, NoteItemDirectiveComponent, EditorDirectiveComponent ],
+      imports: [ QuillModule.forRoot() ]
     })
     .compileComponents();
   }));
