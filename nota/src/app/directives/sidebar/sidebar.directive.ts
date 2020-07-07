b5c3e859cf0f1ef9f21431085af8551975c0f64e
Mitch <me@miitch.io>
2020-07-07 19:51:52 +0800

    Add settings sidebar

--- nota/src/app/directives/sidebar/sidebar.directive.ts
+++ nota/src/app/directives/sidebar/sidebar.directive.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+type MenuType = 'generic' | 'settings';
+
 @Component({
     selector: 'sidebar',
     templateUrl: './sidebar.directive.html'
 })
 export class SidebarDirectiveComponent implements OnInit {
+
+    public activeMenu: MenuType;
+
     ngOnInit() {
+        this.activeMenu = 'generic';
     }
 }
